e35edde898b3c8c87d0faaca8a037fcb97f97bb4
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-06-27 15:51:07 +0400

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,10 +32,20 @@ namespace WPFTreeView
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //Get every logical drive on the machine
             foreach (var drive in Directory.GetLogicalDrives())
             {
+                //Create a new item for it
                 var item = new TreeViewItem();
+
+                //Set the header and path
                 item.Header = drive;
+                item.Tag = drive;
+
+                //Add a dummy item
+                item.Items.Add(null);
+
+                //Add it to the main tree-view
                 FolderView.Items.Add(item);
             }
 
