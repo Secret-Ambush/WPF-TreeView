bd73542f6ff5309f473a64bd4b858a2cd120b852
Riddhi Goswami <91322531+Secret-Ambush@users.noreply.github.com>
2024-06-27 15:41:26 +0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// 
+        #region On Loaded
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem();
+                item.Header = drive;
+                FolderView.Items.Add(item);
+            }
+
+        }
+        #endregion
+        #endregion
+    }
+}
